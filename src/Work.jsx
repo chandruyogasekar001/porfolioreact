8d9f5ceb7638957ff9f5e4d770b9d335439d0261
CHANDRU Y <162431778+chandruyogasekar001@users.noreply.github.com>
2024-06-14 16:22:36 +0530

    Update Work.jsx

--- src/Work.jsx
+++ src/Work.jsx
@@ -5,7 +5,7 @@ import soc from './image/icons8-social-media-logo-48.png';
 import blog from './image/icons8-google-blog-search-48.png';
 export default function Work(){
     return(
-      <div style={{ height: '77vh', width: '85vw', marginTop: '100px', backgroundColor: 'black'}}>
+      <div style={{ height: '77vh', width: '85vw'}}>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>App Icons</title>
@@ -15,63 +15,14 @@ export default function Work(){
           <p>app space</p>
         </header>
         <main>
-          <section className="app-grid">
-            <div className="app-box">
-              <span>
-                <a href="https://chandruyogasekar001.github.io/resumebuilder/" target="blank">
-                  <img
-                    src={res}
-                    alt="Girl in a jacket"
-                    width={100}
-                    height={100}
-                  /><br></br>
-                   </a>
-                  {"resume build-uploaded"}
-                 
-              </span>
-            </div>
-            <div className="app-box">
-              <span>
-               
-                  <img
-                    src={music}
-                    alt="Girl in a jacket"
-                    width={100}
-                    height={100}
-                  /><br></br>
-               {"music app"}
-              </span>
-            </div>
-            <div className="app-box">
-              <span>
-              
-                  <img
-                    src={soc}
-                    alt="Girl in a jacket"
-                    width={100}
-                    height={100}
-                  /><br></br>
-               {"social media app"}
-              </span>
-            </div>
-            <div className="app-box">
-              <span>
-               
-                  <img
-                    src={blog}
-                    alt="Girl in a jacket"
-                    width={100}
-                    height={100}
-                  /><br></br>
-               {"blog app"}
-              </span>
-            </div>
-            
-            
-           
-          
-            
-          </section>
+              <div style={{ textAlign: 'center' }}>
+     
+      <iframe 
+        src="https://dark-matter-web-space.vercel.app/" 
+        style={{ width: '85vw', height: '70vh', border: 'none', marginTop: '10vh' }}
+        title="Dark Matter Web Space"
+      ></iframe>
+    </div>
         </main>
       </div>
       
